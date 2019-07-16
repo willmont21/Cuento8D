e82cd63dcf78641afeaec7692959fb78d2f25177
willmont21 <willmont66@gmail.com>
2019-07-16 09:37:50 -0500

    Creación de Layout de  interacciones
    Layouts de interacciones

---
+++ app/src/main/java/com/example/cuento8d/InteraccionN1.java
@@ -0,0 +1,13 @@
+package com.example.cuento8d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InteraccionN1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interaccion_n1);
+    }
+}
