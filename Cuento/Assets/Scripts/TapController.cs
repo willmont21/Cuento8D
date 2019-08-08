3cdaf1595b894cf6cf3d63e870dde7d1d23ef2fe
willmont21 <willmont66@gmail.com>
2019-08-08 13:26:16 -0500

    lobos

--- Cuento/Assets/Scripts/TapController.cs
+++ Cuento/Assets/Scripts/TapController.cs
@@ -1,33 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TapController : MonoBehaviour
 {
-    public float velocidad = 3f;
+    public Rigidbody2D body;
+    public float walkingSpeed;
+    public Button buttons;
     public GameObject character;
 
-    private Rigidbody2D characterBody;
-
-    // Start is called before the first frame update
     void Start()
     {
-        characterBody = character.GetComponent<Rigidbody2D>();
+        body = character.GetComponent<Rigidbody2D>();
+        Button btns = buttons.GetComponent<Button>();
+        btns.onClick.AddListener(TaskOnClick);
     }
+    
 
-    // Update is called once per frame
     void Update()
     {
-        int i = 0;
-        while (i < Input.touchCount)
-        {
-            RunCharacter(1.0f);
-        }
         
     }
 
-    private void RunCharacter (float horizontalInput)
+    public void TaskOnClick()
     {
-        characterBody.AddForce(new Vector2(horizontalInput * velocidad* Time.deltaTime,0));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(walkingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //var t = collision.gameObject.tag;
+        var otherObject = collision.collider.gameObject;
+        if (otherObject.tag == "loba")
+        {
+
+            SceneManager.LoadScene(8);
+        }
+
+
+
+
+
+
     }
 }
