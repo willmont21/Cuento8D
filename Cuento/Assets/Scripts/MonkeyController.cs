f87210088652cbdf0dbfad3a7ee2951e4162633d
kehylaicaza <keilacicaza@gmail.com>
2019-08-12 01:13:46 -0500

    bad Will

---
+++ Cuento/Assets/Scripts/MonkeyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MonkeyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button  monkeyController; 
+    private int cont=0;
+    void Start()
+    {
+         Button btnMonkey =  monkeyController.GetComponent<Button>();
+         btnMonkey.onClick.AddListener(romperLiandas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+     public void romperLiandas()
+     {
+     cont++;
+     if(cont==7) {
+       SceneManager.LoadScene(12); 
+       }
+      }
+}
---
+++ Cuento/Assets/Scripts/SnakeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SnakeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+ private float speed = 200f;
+ private Rigidbody2D myBody; 
+ private Vector2 vel;
+ public Vector2 Hrange = Vector2.zero;
+ public Vector2 Vrange = Vector2.zero;
+ public Button  snakeController; 
+
+ private bool action= false;
+ private int cont=0;
+      
+     void Start () {
+     myBody = GetComponent<Rigidbody2D> ();
+      vel =myBody.velocity;
+          vel.x =  speed;
+          myBody.velocity = vel;
+        Button btnSnake =  snakeController.GetComponent<Button>();
+         btnSnake.onClick.AddListener(AlejarSerpiente);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      if(transform.position.x >= 650 && action== false){
+             transform.position = new Vector3 (
+              Mathf.Clamp(transform.position.x, Vrange.x,Vrange.y ),
+              Mathf.Clamp(transform.position.y, Hrange.x,Hrange.y ),
+              transform.position.z
+             );
+            
+      }
+	}
+    
+    
+    public void AlejarSerpiente()
+     {
+     cont++;
+     if(cont==7) {
+      action=true;
+      StartCoroutine(retiradaSerpiente());  
+       }
+      }
+      
+    IEnumerator retiradaSerpiente() {  
+        yield return new WaitForSeconds (5);
+        SceneManager.LoadScene(10);
+     }
+    
+   
+}
